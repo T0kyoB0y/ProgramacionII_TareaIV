64aa55cb657f2e7b2139c39ea24cf475b436b34d
MaximoMora <maxmora401@gmail.com>
2023-09-06 13:00:49 -0300

    hola

--- Ejercicio_1/Matriz.py
+++ Ejercicio_1/Matriz.py
@@ -7,50 +7,79 @@ class Matriz:
         self.columnas = columnas
         self.aleatoria = aleatoria
         self.matriz = []
-
-
-    def AddElement(self):
-        for fila in range(0,self.filas):
-            for columna in range(0,self.columnas):
-                self.matriz[i].append(fila + columna)
         
-        print(self.matriz)
+        if aleatoria:
+            temp1 = []
+            for i in range(0, self.filas):
+                temp2 = []
+                for j in range(0, self.columnas):
+                    
+                    temp2.append(random.randint(0, 9))
+                temp1.append(temp2)            
+            self.matriz = temp1
+
+        else:
+            temp1 = []
+            for i in range(0, self.filas):
+                temp2 = []
+                for j in range(0, self.columnas):
+                    
+                    temp2.append(0)
+                temp1.append(temp2)            
+            self.matriz = temp1
+            
     def imprimir(self):
-        for fila in range(0,self.filas):
-            for columna in range(0,self.columnas):
-                print("fila: ", fila)
-                print("columna: ", columna)
+        for i in range(len(self.matriz)):
+            temp = self.matriz[i]
+            print(f"{temp}", end="")
+
+            print("")
 
     def sumar(self,otra):
         nueva_matriz = []
         if self.filas == otra.filas and self.columnas == otra.columnas:
-            for i in range(0,self.filas):
-                for k in range(0,self.columnas):
-                    nuevo_elemento = i + k
-                    print("fila: ", i)
-                    print("columna: ", k)
+            for fila in range(0,self.filas):
+                for columna in range(0,self.columnas):
+                    suma_elemento =  self.matriz[fila][columna] + otra[fila][columna]
+                    nueva_matriz[fila][columna] = suma_elemento
 
-
-                    
-
+        return nueva_matriz
     
     def restar(self,otra):
-        print(otra)
+        nueva_matriz = []
+        if self.filas == otra.filas and self.columnas == otra.columnas:
+            for fila in range(0,self.filas):
+                for columna in range(0,self.columnas):
+                    resta_elemento =  self.matriz[fila][columna] + self.otra[fila][columna]
+                    nueva_matriz[fila][columna] = resta_elemento
 
+        return nueva_matriz
     def multiplicar(self, otra) :
-        print(otra)
+        nueva_matriz = []
+        if self.filas == otra.filas and self.columnas == self.otra.columnas:
+            for fila in range(0,self.filas):
+                for columna in range(0,self.columnas):
+                    continue
+
 
 
 import random
 if __name__ == "__main__":
 
-    matriz_1 = Matriz(2,2,True)
-    matriz_2 = Matriz(2,2,True)
-    #matriz_1.imprimir()
-    matriz_1.sumar(matriz_2)
-
-
-    matriz_x = [[1,2],
-        [3,4]
-    ]
-
+    random.seed(7)
+    matriz1 = Matriz(5, 5, True)
+    random.seed(8)
+    matriz2 = Matriz(5, 5, True)
+    print("Matriz 1:")
+    matriz1.imprimir()
+    print("Matriz 2:")
+    matriz2.imprimir()
+    suma = matriz1.sumar(matriz2)
+    print("Resultado de la suma:")
+    suma.imprimir()
+    #resta = matriz1.restar(matriz2)
+    #print("Resultado de la resta:")
+    #resta.imprimir()
+    #multiplicacion = matriz1.multiplicar(matriz2)
+    #print("Resultado de la multiplicación:")
+    #multiplicacion.imprimir()
